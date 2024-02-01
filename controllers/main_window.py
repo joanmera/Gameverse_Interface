f0537861acef6341bc93ce1e21d3d3591ce3482e
dmeneses11 <daniela.come.papas@gmail.com>
2024-02-01 16:43:33 -0500

    la mainwindow se abre mal no se porque, se redimensiona

--- controllers/main_window.py
+++ controllers/main_window.py
@@ -1,9 +1,12 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QWidget,QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QMessageBox, QPushButton
 from PyQt5 import uic
-
+import pathlib
+from PyQt5 import QtCore
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        pass
+        root_path = pathlib.Path(__file__).parent.parent
+        uic.loadUi(root_path / "views/MainWindow.ui", self)
+
+        
