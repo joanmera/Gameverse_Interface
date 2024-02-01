a3ff063a676ffb9b114315d620cc29734578a88c
Joan Mera <joanmeraramirez@gmail.com>
2024-02-01 13:14:12 -0500

    Initial commit

---
+++ controllers/main_window.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QWidget,QTableWidgetItem
+from PyQt5 import uic
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        pass
